cff5d5d0ecb68fa6f041fb78fdc2578190bff8fc
Bartkooo <bartosz.plochocki@gmail.com>
2023-12-02 19:19:21 +0100

    matches under league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -8,12 +8,19 @@ class LeaguesController < ApplicationController
     @league_name = params[:name]
     @id = params[:id]
     url_teams = URI("https://v3.football.api-sports.io/teams?league=#{@id}&season=2023")
+    url_matches = URI("https://v3.football.api-sports.io/fixtures?status=NS&league=#{@id}&season=2023")
 
     if !REDIS.exists?("teams-#{@id}")
       teams = get_data(url_teams).read_body
       REDIS.set("teams-#{@id}", teams)
     end
+
+    if !REDIS.exists?("matches-#{@id}")
+      matches = get_data(url_matches).read_body
+      REDIS.set("matches-#{@id}", matches)
+    end
     @teams = JSON.parse(REDIS.get("teams-#{@id}"))['response']
+    @matches = JSON.parse(REDIS.get("matches-#{@id}"))['response']
   end
 
   private
