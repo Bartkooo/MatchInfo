ad5682a00a1c2ccb5368e56ff66914697d7ee3c2
Bartkooo <bartosz.plochocki@gmail.com>
2023-12-04 17:15:08 +0100

    teams controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :leagues, only: [:index, :show]
+  resources :teams, only: [:show]
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
