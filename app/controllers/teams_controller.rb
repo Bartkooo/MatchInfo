2e31097d770fff43ca27ca6afd79b06bf3299a53
Bartkooo <bartosz.plochocki@gmail.com>
2023-12-04 17:44:47 +0100

    team styling

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,15 @@
 class TeamsController < ApplicationController
   def show
+    @team_id = params[:team_id]
+    @team_name = params[:team_name]
+
+    url_team = URI("https://v3.football.api-sports.io/teams?id=#{@team_id}")
+
+    if !REDIS.exists?("team-#{@team_id}")
+      team = get_data(url_team).read_body
+      REDIS.set("team-#{@team_id}", team)
+    end
+
+    @team = JSON.parse(REDIS.get("team-#{@team_id}"))['response']
   end
 end
