b1ddb37f80ceb5d14592640348e9cf828942d7e6
Bartkooo <bartosz.plochocki@gmail.com>
2023-12-04 18:02:39 +0100

    root page, leagues list styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :leagues, only: [:index, :show]
+  resources :leagues, only: [:show]
   resources :teams, only: [:show]
-  root 'home#index'
+  root 'leagues#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
