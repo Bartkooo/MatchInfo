bb3941130684bc34e24635fb010806fb4f5ecb30
Bartkooo <bartosz.plochocki@gmail.com>
2023-12-05 08:35:03 +0100

    players

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,15 +1,30 @@
 class TeamsController < ApplicationController
+  before_action :set_redis, only: [:show]
+
   def show
     @team_id = params[:team_id]
     @team_name = params[:team_name]
 
     url_team = URI("https://v3.football.api-sports.io/teams?id=#{@team_id}")
+    url_players = URI("https://v3.football.api-sports.io/players/squads?team=#{@team_id}")
 
     if !REDIS.exists?("team-#{@team_id}")
       team = get_data(url_team).read_body
       REDIS.set("team-#{@team_id}", team)
     end
 
+    if !REDIS.exists?("players-#{@team_id}")
+      players = get_data(url_players).read_body
+      REDIS.set("players-#{@team_id}", players)
+    end
+
     @team = JSON.parse(REDIS.get("team-#{@team_id}"))['response']
+    @players = JSON.parse(REDIS.get("players-#{@team_id}"))['response']
+  end
+
+  private
+
+  def set_redis
+    REDIS.select(1)
   end
 end
